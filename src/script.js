704469b2a60920ffd659231433af35bc9be8e52d
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-03-31 22:26:50 -0700

    Revert "update js to fix the ball/css to fix rick"
    This reverts commit 5b187f2c7032ef8f5f5870d23fc428132be79820.

--- src/script.js
+++ src/script.js
@@ -10,37 +10,34 @@ function headerClick() {
 	}
 }
 
-var currentCur = {x:20,y:50};
-var prevCur = {x:20,y:50};
+var currentCur = {x:20,y:20};
+var prevCur = {x:20,y:20};
 var GravPos = {x:100,y:100};
 
 document.body.addEventListener("mousemove",moveGravCenter, false);
 document.body.addEventListener("mouseleave",moveGravCenter, false);
 document.body.addEventListener("mouseenter",moveGravCenter, false);
 
-setInterval(computeCursorBallPhysic,0.001);
+setInterval(computeCursorBallPhysic,0.001)
 
 function computeCursorBallPhysic() {
 	let timeStep = 0.05;
-	let gravConst = 10000000000;
+	let gravConst = 1000000;
 	let tempPrevPos = {x: currentCur.x, y: currentCur.y};
 	let distanceToCen = Math.sqrt(Math.pow(currentCur.x - GravPos.x,2) + Math.pow(GravPos.y - currentCur.y,2));
-	let VectToCen = {x: (currentCur.x - GravPos.x)/distanceToCen, y: (currentCur.y - GravPos.y)/distanceToCen};
+	let VectToCen = {x: (GravPos.x - currentCur.x)/distanceToCen, y: (currentCur.y - GravPos.y)/distanceToCen};
 	let velocity = {x: (currentCur.x - prevCur.x), y: (currentCur.y - prevCur.y)};
-	//let Acc = {x: -(gravConst/(distanceToCen*distanceToCen))*(VectToCen.x), y:(gravConst/(distanceToCen*distanceToCen))*(VectToCen.y)};
-	let ballVelMag = Math.sqrt(Math.pow(velocity.x,2)+Math.pow(velocity.y,2));
-	let Acc = {x: VectToCen.x*-5, y:VectToCen.y*5};
-	if (ballVelMag!=0) {
-		Acc = {x: Acc.x - ((velocity.x/ballVelMag)*Math.pow(ballVelMag,2)*0.03), y: Acc.y + ((velocity.y/ballVelMag)*Math.pow(ballVelMag,2)*0.03)};
-	}
-	Acc.x = Math.min(Math.max(Acc.x,-50),50);
-	Acc.y = Math.min(Math.max(Acc.y,-50),50);
+	let Acc = {x: gravConst/(distanceToCen*distanceToCen+0.1)*VectToCen.x, y:gravConst/(distanceToCen*distanceToCen+0.1)*-VectToCen.y};
+	//let Acc = {x: VectToCen.x*0.9 - velocity.x*0.3, y:VectToCen.y*-0.9 - velocity.y*0.3};
+	Acc = {x: Acc.x - velocity.x*0.3, y: Acc.y - velocity.y*0.3};
+	Acc.x = Math.min(Math.max(Acc.x,-15),15);
+	Acc.y = Math.min(Math.max(Acc.y,-15),15);
+
 
 	currentCur.x = 2 * currentCur.x - prevCur.x + Acc.x * (timeStep*timeStep);
-	currentCur.y = 2 * currentCur.y - prevCur.y + -Acc.y * (timeStep*timeStep);
+	currentCur.y = 2 * currentCur.y - prevCur.y + Acc.y * (timeStep*timeStep);
 	prevCur = {x: tempPrevPos.x, y: tempPrevPos.y};
 
-	//keep in bound
 	currentCur = {x: Math.min(Math.max(currentCur.x,20),window.innerWidth-20),y: Math.min(Math.max(currentCur.y,0),window.innerHeight-20)};
 
 	let ball = document.getElementById("ball");
