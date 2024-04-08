6ce3ff447448041b15de153907a265b6058a01cd
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-04-08 14:14:19 -0700

    rick will autoresize based on client

--- docs/script.js
+++ docs/script.js
@@ -2,8 +2,8 @@ function headerClick() {
 	var iframe = document.createElement("iframe");
 	if (document.getElementById("rickplayer") === null) {
 		iframe.id = "rickplayer";
-		iframe.width = 400;
-		iframe.height = 400;
+		iframe.width = document.getElementById('rickBody').clientWidth * 0.9;
+		iframe.height = document.getElementById('rickBody').clientWidth * 0.9;
 		iframe.src = "https://www.youtube.com/embed/uHgt8giw1LY?autoplay=1&mute=1&controls=0"; 
 		var body = document.getElementById("rickBody");
 		body.appendChild(iframe);
